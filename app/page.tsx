030002df414dbb6205fb93f1e58ceb053f55dcf3
weichaozhan <921391554@qq.com>
2024-10-05 20:16:40 +0800

    page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <p>This is a Test page</p>
+        <a href="/about" >about</a>
       </div>
     </main>
   );
