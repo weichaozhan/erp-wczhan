47c2a9624c7c13c009ff10b3d1d3d45220614d69
weichaozhan <921391554@qq.com>
2024-10-27 23:15:20 +0800

    texture & draggable

--- app/designer/components/ThreeScene.tsx
+++ app/designer/components/ThreeScene.tsx
@@ -3,24 +3,15 @@
 import _ from 'lodash';
 import classNames from 'classnames';
 import * as Three from 'three';
+import interact from 'interactjs';
 import { useCallback, useEffect, useRef } from 'react';
-import { Button } from 'antd';
-import { GLTFLoader } from 'three/examples/jsm/Addons.js';
 
 import styles from '../styles/threeScene.module.scss';
 
-interface ThreeObj {
-  parrot?: Three.Object3D<Three.Object3DEventMap> & { tick?: (time: number) => void };
-  cube?: Three.Mesh<Three.BoxGeometry, Three.MeshBasicMaterial, Three.Object3DEventMap>;
-  line?: Three.Line<Three.BufferGeometry<Three.NormalBufferAttributes>, Three.LineBasicMaterial, Three.Object3DEventMap>;
-  renderer?: Three.WebGLRenderer;
-  scene?: Three.Scene;
-  camera?: Three.PerspectiveCamera;
-}
+import { ThreeObj, createParrot } from '../handlers/modelCreators';
 
 const ThreeScene = () => {
   const wrapperDom = useRef<Element | null>();
-  const animeTimer = useRef<number | null | undefined>();
   const resizeTarget = useRef<EventListenerObject['handleEvent']>();
 
   // Three 帧时时钟
@@ -28,124 +19,56 @@ const ThreeScene = () => {
 
   const threeObj = useRef<ThreeObj>({});
 
-  const animate = useCallback(() => {
-        const { parrot, cube, line, renderer, scene, camera } = threeObj.current;
-        
-        if (animeTimer.current === null) {
-          animeTimer.current = undefined;
-          return;
-        }
+  const rerenderCanvas = () => {
+    const { renderer, camera, scene } = threeObj.current;
 
-        if (renderer && scene && camera) {
-          if (cube || line || parrot) {
-            animeTimer.current = requestAnimationFrame(() => {
-              animate();
-            });
-            renderer.render(scene, camera);
-          }
-          // 立方体旋转
-          if (cube) {
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
-            cube.rotation.z += 0.01;
-          }
-
-          if (line) {
-            line.rotation.x -= 0.02;
-            line.rotation.y -= 0.01;
-            line.rotation.z += 0.01;
-          }
-
-          if (parrot) {
-            parrot.rotation.y += 0.01;
-            if (clockThree.current) {
-              parrot.tick?.(clockThree.current.getDelta());
-            }
-          }
-        }
-  }, []);
+    if (renderer && scene && camera) {
+      renderer.render(scene, camera);
+    }
+  };
 
   const handleResize = () => {
     resizeTarget.current = () => {
-      if (wrapperDom.current && threeObj.current.camera) {
+      const { renderer, camera, scene } = threeObj.current;
+      if (wrapperDom.current && camera) {
         const width = wrapperDom.current.clientWidth;
         const height = wrapperDom.current.clientHeight;
 
         threeObj.current.renderer?.setSize(width, height);
-        threeObj.current.camera.aspect = width / height;
-        threeObj.current.camera.updateProjectionMatrix();
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+
+        rerenderCanvas();
       }
     };
     window.addEventListener('resize', resizeTarget.current);
   };
 
-  // 立方体
-  const createCube = () => {
-    const geometry = new Three.BoxGeometry(1, 1, 1);
-    const material = new Three.MeshBasicMaterial({ color: 0x00ff00 });
-    threeObj.current.cube = new Three.Mesh(geometry, material);
+  const dragHandler = () => {
+    interact('#three-scene').draggable({
+      listeners: {
+        move: (e) => {
+          const { parrot } = threeObj.current;
+          const { x, y } = e.delta ?? {};
+          
+          if (clockThree.current && parrot) {
+            const texture = new Three.TextureLoader().load('/robot.png');
+            const material = new Three.MeshBasicMaterial({
+              map: texture,
+            });
+            parrot.material = material;
 
-    threeObj.current.scene?.add(threeObj.current.cube);
-  }
-
-  // 画线
-  const createLine = () => {
-    const material = new Three.LineBasicMaterial({
-      color: 'red',
+            if (x) {
+              parrot.rotation.y += (x / 10);
+            }
+            if (y) {
+              parrot.rotation.x += (y / 10);
+            }
+            rerenderCanvas();
+          }
+        },
+      },
     });
-
-    const points: Three.Vector3[] = [];
-    points.push(new Three.Vector3(-2, 0, 0));
-    points.push(new Three.Vector3(0, 3, 0));
-    points.push(new Three.Vector3(2, 0, 0));
-
-    const geometry = new Three.BufferGeometry().setFromPoints(points);
-
-    threeObj.current.line = new Three.Line(geometry, material);
-
-    threeObj.current.scene?.add(threeObj.current.line);
-  }
-
-  // bird
-  const createParrot = async () => {
-    const { scene, camera, renderer } = threeObj.current;
-    const loader = new GLTFLoader();
-
-    if (scene && camera && renderer) {
-      const parrotData = await loader.loadAsync('/model3d/Parrot.glb');
-      
-      // get model
-      threeObj.current.parrot = parrotData.scene.children[0];
-
-      // get animation
-      const anime = parrotData.animations[0];
-      // declare mixer
-      const mixer = new Three.AnimationMixer(threeObj.current.parrot);
-      // 将动画按照动作进行动画剪辑
-      const action = mixer.clipAction(anime);
-      action.play();
-  
-      threeObj.current.parrot.tick = (delta: number) => {
-        mixer.update(delta);
-      };
-
-      threeObj.current.parrot.scale.set(0.05, 0.05, 0.05);
-
-      threeObj.current.parrot.position.set(-6, 0, -2);
-
-      // DirectionalLight: 直射光，可以理解为手电筒，从放置处发出平行光。缺点也比较明显，只能照向一个位置，而且直射光很浪费性能，不建议使用太多。
-      // AmbientLight: 环境光，从各个方向向场景中的每个对象添加恒定数量的光照，与现实中光的工作模式完全不同。用起来很简单，通常来配合 DirectionalLight 来使用。但环境光由于各方向相同，无法显示出深度信息。
-      // HemisphereLight: 半球光，光源在场景顶部的天空颜色和场景底部的地面颜色之间渐变，比较接近现实的光。半球光性能非常高，但其不从某一特定方向照射，因此通常和直射光配合使用实现某区域的高光效果。
-      // 设置直射光
-      const mainLight = new Three.DirectionalLight();
-      // 设置半球光
-      const enLight = new Three.HemisphereLight();
-
-      scene.add(threeObj.current.parrot, mainLight, enLight);
-      renderer.render(scene, camera);
-      clockThree.current = new Three.Clock();
-    }
-
   };
 
   const initThree = useCallback(_.once(async () => {
@@ -166,51 +89,17 @@ const ThreeScene = () => {
       threeObj.current.renderer.setSize( width, height );
       wrapperDom.current.appendChild(threeObj.current.renderer.domElement);
 
-      threeObj.current.camera.position.z = 10;
+      threeObj.current.camera.position.z = 6;
 
-      createCube();
-      createLine();
-      await createParrot();
-      animate();
+      await createParrot({ threeObj: threeObj.current });
+
+      clockThree.current = new Three.Clock();
 
       handleResize();
+
+      dragHandler();
     }
   }), []);
-
-  const stop = (position?: {
-    x: number,
-    y: number,
-    z: number,
-  } | undefined) => {
-    animeTimer.current && cancelAnimationFrame(animeTimer.current);
-
-    animeTimer.current = null;
-
-    if (position) {
-      const { x, y, z } = position;
-      requestAnimationFrame(() => {
-        const {cube, renderer, scene, camera} = threeObj.current;
-        
-        if (cube && renderer && scene && camera) {
-          cube.rotation.x = x;
-          cube.rotation.y = y;
-          cube.rotation.z = z;
-          renderer.render(scene, camera);
-        }
-      });
-    }
-  };
-
-  const reset = () => {
-    stop({ x: 0, y: 0, z: 0 });
-  };
-
-  const play = () => {
-    if (animeTimer.current === null) {
-      animeTimer.current = undefined;
-      animate();
-    }
-  };
 
   useEffect(() => {
     wrapperDom.current = document.querySelector('#three-scene');
@@ -220,15 +109,6 @@ const ThreeScene = () => {
 
   return (
     <div className={classNames(styles['three-content-wrapper'])} >
-      <div className={classNames(styles['tools-wrapper'])}>
-        <Button onClick={reset}>
-          重置
-        </Button>
-
-        <Button onClick={play}>
-          播放
-        </Button>
-      </div>
       <div id="three-scene" className={classNames(styles['three-scene'])} >
       </div>
     </div>
---
+++ app/designer/handlers/modelCreators.ts
@@ -0,0 +1,61 @@
+import * as Three from 'three';
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
+
+export interface ThreeObj {
+  parrot?: Three.Object3D & { tick?: (time: number) => void, material?: Three.MeshBasicMaterial };
+  cube?: Three.Mesh<Three.BoxGeometry, Three.MeshBasicMaterial, Three.Object3DEventMap>;
+  line?: Three.Line<Three.BufferGeometry<Three.NormalBufferAttributes>, Three.LineBasicMaterial, Three.Object3DEventMap>;
+  renderer?: Three.WebGLRenderer;
+  scene?: Three.Scene;
+  camera?: Three.PerspectiveCamera;
+}
+
+// bird
+export interface CreateParrotParmas {
+  threeObj: ThreeObj;
+}
+export const createParrot = async ({
+  threeObj,
+}: CreateParrotParmas) => {
+  const { scene, camera, renderer } = threeObj;
+  const loader = new GLTFLoader();
+
+
+  if (scene && camera && renderer) {
+    const parrotData = await loader.loadAsync('/model3d/Parrot.glb');
+    
+    // get model
+    threeObj.parrot = parrotData.scene.children[0];
+
+    const texture = new Three.TextureLoader().load('/robot.png');
+    const material = new Three.MeshBasicMaterial({
+      map: texture,
+    });
+    threeObj.parrot.material = material;
+
+    // get animation
+    const anime = parrotData.animations[0];
+    // declare mixer
+    const mixer = new Three.AnimationMixer(threeObj.parrot);
+    // 将动画按照动作进行动画剪辑
+    const action = mixer.clipAction(anime);
+    action.play();
+
+    threeObj.parrot.tick = (delta: number) => {
+      mixer.update(delta);
+    };
+
+    threeObj.parrot.scale.set(0.05, 0.05, 0.05);
+
+    // DirectionalLight: 直射光，可以理解为手电筒，从放置处发出平行光。缺点也比较明显，只能照向一个位置，而且直射光很浪费性能，不建议使用太多。
+    // AmbientLight: 环境光，从各个方向向场景中的每个对象添加恒定数量的光照，与现实中光的工作模式完全不同。用起来很简单，通常来配合 DirectionalLight 来使用。但环境光由于各方向相同，无法显示出深度信息。
+    // HemisphereLight: 半球光，光源在场景顶部的天空颜色和场景底部的地面颜色之间渐变，比较接近现实的光。半球光性能非常高，但其不从某一特定方向照射，因此通常和直射光配合使用实现某区域的高光效果。
+    // 设置直射光
+    const mainLight = new Three.DirectionalLight();
+    // 设置半球光
+    const enLight = new Three.HemisphereLight();
+
+    scene.add(threeObj.parrot, mainLight, enLight);
+    renderer.render(scene, camera);
+  }
+};
