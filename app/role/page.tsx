64f835563a584acec8b533aa215eb1489c0f77fb
weichaozhan <921391554@qq.com>
2025-04-16 15:32:05 +0800

    feature: del role

--- app/role/page.tsx
+++ app/role/page.tsx
@@ -6,18 +6,30 @@ import { FC, useRef, useState } from 'react';
 import globalStyles from '../global.module.scss';
 
 import RoleTable from './components/RoleTable';
-import { Button, Divider } from 'antd';
+import { App, Button, Divider } from 'antd';
 import RoleForm from './components/forms/RoleForm';
 import { Role } from '../types/entity';
 import { RoleTableRef } from '../components/AuthTree/types';
+import { delRoleApi } from '../api/auth';
 
 const RolePage: FC = () => {
+  const {message } = App.useApp();
+
   const tableRef = useRef<RoleTableRef | null>();
 
   const [isEdit, setIsEdit] = useState(false);
   const [roleVisible, setRoleVisible] = useState(false);
 
   const [roleData, setRoleData] = useState<Role>();
+
+  const delRole = async (role: Role) => {
+    try {
+      await delRoleApi(role.id);
+      message.success(`删除【${role.nameToShow}】成功!`);
+    } catch (e) {
+      message.error(Object.toString.call(e));
+    }
+  };
 
   return <>
     <div
@@ -40,6 +52,7 @@ const RolePage: FC = () => {
           setRoleData(role);
           setIsEdit(true);
         }}
+        onDel={delRole}
       />
     </div>
 
